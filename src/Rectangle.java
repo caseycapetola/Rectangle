2d139ad81473fd9d01ed0c1ba839ba10ed86d758
ccapetola20 <42975602+ccapetola20@users.noreply.github.com>
2019-02-04 12:40:43 -0500

    Add files via upload

---
+++ src/Rectangle.java
@@ -0,0 +1,20 @@
+
+public class Rectangle 
+{
+
+	private int dimensionX = 0;
+	private int dimensionY = 0;
+	
+		//fields for dimensions only
+		
+		//methods
+		//setWidth
+		//setHeight
+		//getWidth
+		//getHeight
+		//getArea
+		//getDiagonal
+
+	
+
+}
